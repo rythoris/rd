f2576346074160e87446e75266555a16643f78f3
rythoris <139784947+rythoris@users.noreply.github.com>
2024-01-18 15:13:01 +0330

    Introduce print format for the `list` sub-command

--- cmd/rd/cmd_list.go
+++ cmd/rd/cmd_list.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
-
-	"github.com/charmbracelet/lipgloss"
-	"github.com/charmbracelet/lipgloss/table"
+	"time"
 
 	"github.com/rythoris/rd"
 	"github.com/rythoris/rd/internal/config"
 )
 
 type ListCommand struct {
-	Raw bool `arg:"-r,--raw" help:"print raw data as tsv"`
+	Format   PrintFormat `arg:"-f,--format" help:"print fromat (possible values: jsonl, json_array, tsv)" default:"tsv"`
+	NoHeader bool        `arg:"-n,--no-header" help:"do not print tsv header (only effects the tsv print format)" default:"false"`
 }
 
 func (c ListCommand) Run(config config.Config) int {
@@ -23,18 +24,63 @@ func (c ListCommand) Run(config config.Config) int {
 		return 1
 	}
 
-	if c.Raw {
-		for _, r := range rds {
-			fmt.Printf("%d\t%s\t%s\t%s\n", r.ID, r.Title, r.Link, strings.Join(r.Tags, ","))
+	switch c.Format {
+	case FormatTsv:
+		if !c.NoHeader {
+			fmt.Printf("id\tcreated\tmodified\ttitle\tlink\ttags\n")
 		}
-	} else {
-		t := table.New().
-			Border(lipgloss.HiddenBorder()).
-			Headers("ID", "Title", "URL", "Tags")
 		for _, r := range rds {
-			t.Row(fmt.Sprintf("%d", r.ID), r.Title, r.Link, strings.Join(r.Tags, ","))
+			fmt.Printf(
+				"%d\t%s\t%s\t%s\t%s\t%s\n",
+				r.ID,
+				r.Created.Format(time.ANSIC),
+				r.Modified.Format(time.ANSIC),
+				r.Title,
+				r.Link,
+				strings.Join(r.Tags, ","),
+			)
 		}
-		fmt.Println(t)
+	case FormatJsonLine:
+		sb := strings.Builder{}
+		for _, r := range rds {
+			b, err := json.Marshal(r)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "[!] ERROR: marshal error: %s\n", err.Error())
+				return 1
+			}
+			sb.WriteString(string(b) + "\n")
+		}
+		fmt.Print(sb.String())
+	case FormatJsonArray:
+		b, err := json.MarshalIndent(rds, "", "  ")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[!] ERROR: marshal error: %s\n", err.Error())
+			return 1
+		}
+		fmt.Println(string(b))
 	}
 	return 0
+}
+
+type PrintFormat uint
+
+const (
+	FormatJsonLine PrintFormat = iota
+	FormatJsonArray
+	FormatTsv
+)
+
+var printFormatNames = [...]string{
+	FormatJsonLine:  "jsonl",
+	FormatJsonArray: "json_array",
+	FormatTsv:       "tsv",
+}
+
+func (f *PrintFormat) UnmarshalText(data []byte) error {
+	i := slices.Index(printFormatNames[:], strings.ToLower(string(data)))
+	if i == -1 {
+		return fmt.Errorf("invalid print format: %s", string(data))
+	}
+	*f = PrintFormat(i)
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,20 +2,11 @@ module github.com/rythoris/rd
 
 go 1.21.4
 
-require (
-	github.com/adrg/xdg v0.4.0
-	github.com/charmbracelet/lipgloss v0.9.1
-)
+require github.com/adrg/xdg v0.4.0
 
 require (
 	github.com/alexflint/go-scalar v1.1.0 // indirect
-	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
-	github.com/muesli/reflow v0.3.0 // indirect
-	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	golang.org/x/term v0.14.0 // indirect
 )
